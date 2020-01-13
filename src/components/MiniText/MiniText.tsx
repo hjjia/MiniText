4570cfa5a66f91eef19f6118632de1397a08d4b0
hjjia <normal999@163.com>
2020-01-13 19:01:58 +0800

    add opts bar

--- src/components/MiniText/MiniText.tsx
+++ src/components/MiniText/MiniText.tsx
@@ -1,59 +1,115 @@
 import React from 'react';
-const optsArr = [{
-    key: 'blod',
-    title: '加粗'
+
+/**
+ * 1：浏览器execCommand自带的命令
+ * 2：需要通过 insertHTML 命令来实现的
+ * 3：创建链接 createLink
+ * 4：insertImage
+ */
+const OptTypeObj = {
+    auto: 1,
+    insertHTML: 2,
+    createLink: 3,
+    image: 4,
+}
+const OptTypeValues = 1 | 2 | 3 | 4;
+interface OptType {
+    key: string;
+    title: string;
+    type?: typeof OptTypeValues ; // 用于判断类型 
+    isActive?: boolean;
+    execCommandKey?: string; // 用于修改样式的命令
+}
+const optsArr: OptType[] = [{
+    key: 'bold',
+    title: '加粗',
+    execCommandKey: 'bold',
+    type: OptTypeObj.auto,
 }, {
     key: 'italic',
-    title: '斜体'
+    title: '斜体',
+    execCommandKey: 'italic',
+    type: OptTypeObj.auto,
 }, {
     key: 'underline',
-    title: '下划线'
+    title: '下划线',
+    execCommandKey: 'underline',
+    type: OptTypeObj.auto,
 }, {
     key: 'code',
-    title: 'code'
+    title: 'code',
+    execCommandKey: 'insertHTML',
+    type: OptTypeObj.insertHTML,
 }, {
     key: 'upload',
-    title: '上传图片'
+    title: '上传图片',
+    execCommandKey: 'insertImage',
+    type: OptTypeObj.image,
 }, {
     key: 'link',
-    title: '添加链接'
+    title: '添加链接',
+    execCommandKey: 'createLink',
+    type: OptTypeObj.createLink,
 }, {
     key: 'sup',
-    title: '上标'
+    title: '上标',
+    execCommandKey: 'insertHTML',
+    type: OptTypeObj.insertHTML,
 }, {
     key: 'sub',
-    title: '下标'
+    title: '下标',
+    execCommandKey: 'insertHTML',
+    type: OptTypeObj.insertHTML,
 }, {
     key: 'undo',
-    title: '撤销'
+    title: '撤销',
+    execCommandKey: 'bold',
+    type: OptTypeObj.auto,
 }, {
     key: 'redo',
-    title: '重做'
+    title: '重做',
+    execCommandKey: 'bold',
+    type: OptTypeObj.auto,
 }, {
     key: 'align-left',
-    title: '左对齐'
+    title: '左对齐',
+    execCommandKey: 'justifyLeft',
+    type: OptTypeObj.auto,
 }, {
     key: 'align-center',
-    title: '居中'
+    title: '居中',
+    execCommandKey: 'justifyCenter',
+    type: OptTypeObj.auto,
 }, {
     key: 'align-right',
-    title: '右对齐'
+    title: '右对齐',
+    execCommandKey: 'justifyRight',
+    type: OptTypeObj.auto,
 }, {
     key: 'list-ul',
-    title: '项目符号'
+    title: '项目符号',
+    execCommandKey: 'insertHTML',
+    type: OptTypeObj.insertHTML,
 }, {
     key: 'list-ol',
-    title: '编号'
+    title: '编号',
+    execCommandKey: 'insertHTML',
+    type: OptTypeObj.insertHTML,
 }, {
     key: 'blockquote',
-    title: '引用'
+    title: '引用',
+    execCommandKey: 'insertHTML',
+    type: OptTypeObj.insertHTML,
 }];
 
-function conversetOptToMap(arr: any[]): any {
+function conversetOptToMap(arr: OptType[]): any {
     const res = new Map();
     for (let i = 0, len = arr.length; i < len; i++) {
         const { key, title } = arr[i];
-        res.set(key.replace('-',''), arr[i]);
+        res.set(key, {
+            ...arr[i],
+            isActive: false,
+        });
     }
     return res;
 }
@@ -61,7 +117,12 @@ function conversetOptToMap(arr: any[]): any {
 export default class MiniText extends React.Component<any, any> {
     state = {
         optsMap: conversetOptToMap(optsArr),
+        isMiniTextFocus: false,
     }
+
+    /**
+     * 渲染操作栏
+     */
     getOptsList = () => {
         const { optsMap } = this.state;
         const keys = [...optsMap.keys()];
@@ -71,16 +132,67 @@ export default class MiniText extends React.Component<any, any> {
 
         return keys.map((k: string) => {
             const item = optsMap.get(k);
-            const { key, title } = item;
+            const { key, title, isActive } = item;
             return (
-                <div className="mini-text-opts-item" title={title} key={key}>
+                <span
+                    className={`mini-text-opts-item ${isActive && 'mini-text-opts-item-active'}`}
+                    title={title} key={key}
+                    onClick={() => this.handleOptChange(item)}
+                >
                     <span className={`iconfont icon-${key}`}></span>
-                </div>
+                </span>
             )
         })
     }
 
+    /**
+     * 处理操作栏
+     */
+    handleOptChange = (item: OptType) => {
+        const { key, type } = item;
+
+        switch (type) {
+            case OptTypeObj.auto:
+                this.handleAutoCommand(item);
+                break;
+        }
+        
+    }
+
+    /**
+     * 处理浏览器自动执行的样式修改
+     */
+    handleAutoCommand = (item: OptType) => {
+        const { key } = item;
+        const { optsMap } = this.state;
+        this.execCommandStyles(key);
+        optsMap.set(key, {
+            ...item,
+            isActive: !item.isActive,
+        })
+        this.setState({
+            optsMap,
+        })
+    }
+
+    /**
+     * 修改样式
+     */
+    execCommandStyles = (command: string, val?: any) => {
+        document.execCommand('StyleWithCSS',true); // 用行内样式修改样式 否则就用 标签方式
+        document.execCommand(command,false,val);
+    }
+
+    handleMiniTextFocus = () => {
+        this.setState({
+            isMiniTextFocus: true,
+        });
+
+        console.log(document.queryCommandEnabled("bold"), 'document.queryCommandEnabled("bold");')
+    }
+
     render() {
+        const { isMiniTextFocus } = this.state;
         return (
             <div className="mini-text-root">
                 <div className="mini-text-wrap">
@@ -88,9 +200,10 @@ export default class MiniText extends React.Component<any, any> {
                         {this.getOptsList()}
                     </div>
                     <div 
-                        className="mini-text-content" 
+                        className={`mini-text-content ${isMiniTextFocus && 'mini-text-content-focus'}`} 
                         contentEditable={true} 
                         suppressContentEditableWarning={true}
+                        onFocus={this.handleMiniTextFocus}
                     >
                         <p> </p>
                     </div>
